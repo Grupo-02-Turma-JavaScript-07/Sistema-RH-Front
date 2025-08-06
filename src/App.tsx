cef61128f8ad68657286c3015654693c3b149702
Natalia Taira <nataliagtaira@gmail.com>
2025-08-06 11:46:53 -0300

    Adiciona rota para as páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaColaborador from './components/colaboradores/listacolaborador/ListaColaborador'
+import Footer from './components/footer/Footer'
+import Sidebar from './components/sidebar/Sidebar'
+import Perfil from './pages/perfil/Perfil'
+import Sobre from './pages/sobre/Sobre'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Sidebar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/colaboradores" element={<ListaColaborador />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/perfil" element={<Perfil />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
