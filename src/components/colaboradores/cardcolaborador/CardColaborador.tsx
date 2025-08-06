25f0ac22d65516bd3912de6078f9fb9a1f261df4
NataliaGadelha <natt.gdl25@gmail.com>
2025-08-06 16:32:37 -0300

    style: alterando a estilização do cardColaborador

--- src/components/colaboradores/cardcolaborador/CardColaborador.tsx
+++ src/components/colaboradores/cardcolaborador/CardColaborador.tsx
@@ -9,18 +9,8 @@ function CardColaborador({ colaborador }: Props) {
   const dataFormatada = new Date(colaborador.dataAdmissao).toLocaleDateString("pt-BR");
 
   return (
-    <div className="flex-1 p-6 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <div className="flex justify-end px-4 pt-4">
-        <button
-          id={`dropdownButton-${colaborador.id}`}
-          data-dropdown-toggle={`dropdown-${colaborador.id}`}
-          className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
-          type="button"
-        >
-        </button>
-  
-      </div>
-      <div className="flex flex-col items-center pb-10">
+    <div className=" p-6 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-col items-center pb-3">
         <img
           className="w-30 h-30 mb-3 rounded-full shadow-lg object-cover"
           src={colaborador.foto}
@@ -38,16 +28,16 @@ function CardColaborador({ colaborador }: Props) {
         <span className="text-sm text-gray-500 dark:text-gray-400">
           Salário: R$ {colaborador.salario.toFixed(2)}
         </span>
-        <div className="flex mt-4 md:mt-6">
+        <div className="flex mt-2 md:mt-6 ">
           <a
             href="#"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-3 py-1.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Contato
           </a>
           <a
             href="#"
-            className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            className="py-1.5 px-3 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
             Mensagem
           </a>
