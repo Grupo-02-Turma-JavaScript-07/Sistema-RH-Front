ca8b13621f3d8f952251c1d5db6abd95b737c208
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-06 23:50:58 -0300

    chore: adicionando lógica para os botoes da side bar conforme a rota

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react"
 import { FaHome, FaUser } from "react-icons/fa"
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io"
+import { Link, useLocation } from "react-router-dom"
 
 function Sidebar() {
   const [abaColaboradores, setAbaColaboradores] = useState(false)
+  const location = useLocation()
+  const { pathname } = location
+
+  const inicioEstaAtivo = pathname === "/perfil" || pathname === "/home" || pathname === "/"
+  const colaboradoresEstaAtivo = pathname.startsWith("/colaboradores")
 
   return (
     <div className="flex ">
@@ -12,15 +18,20 @@ function Sidebar() {
           RH - FLOW
         </div>
         <div className="w-full h-px bg-indigo-100 mb-2" />
-        <div className="relative flex items-center gap-2 bg-indigo-100 text-indigo-900 rounded-md pl-4 pr-3 py-2 mb-1">
-          <span className="absolute left-0 top-1 bottom-1 w-1 bg-indigo-600 rounded-r-md" />
-          <FaHome size={14} />
-          <span>Início</span>
-        </div>
+
+        <Link to="/perfil">
+          <div className={`relative flex items-center gap-2 px-4 py-2 mb-1 rounded-md cursor-pointer ${inicioEstaAtivo ? 
+            "bg-indigo-100 text-indigo-900" : "text-gray-700 hover:bg-gray-100"}`}>
+            {inicioEstaAtivo && <span className="absolute left-0 top-1 bottom-1 w-1 bg-indigo-600 rounded-r-md" />}
+            <FaHome size={14} />
+            <span>Início</span>
+          </div>
+        </Link>
 
         <div
           onClick={() => setAbaColaboradores(!abaColaboradores)}
-          className="flex items-center justify-between text-gray-700 hover:bg-gray-100 cursor-pointer px-4 py-2 rounded-md"
+          className={`flex items-center justify-between px-4 py-2 rounded-md cursor-pointer 
+          ${colaboradoresEstaAtivo ? "bg-indigo-100 text-indigo-900" : "text-gray-700 hover:bg-gray-100"}`}
         >
           <div className="flex items-center gap-2">
             <FaUser size={14} />
@@ -31,7 +42,10 @@ function Sidebar() {
 
         {abaColaboradores && (
           <div className="ml-10 mt-1 flex flex-col gap-1 text-gray-600 text-sm">
-            <span className="hover:text-indigo-600 cursor-pointer">Listar</span>
+            <Link to="/colaboradores" className={`hover:text-indigo-600 ${pathname === "/colaboradores" ? "text-indigo-700 font-semibold" : ""}`}>
+              Listar
+            </Link>
+            {/* ##todo - vamos implementar o cadastro? */}
             <span className="hover:text-indigo-600 cursor-pointer">Cadastrar</span>
           </div>
         )}
